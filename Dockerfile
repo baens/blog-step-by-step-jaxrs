cb1373152bf52d7cdf5aa08489bafc327cf7912e
Erik Lindblom <erik@baens.net>
2018-02-16 05:30:21 -0700

    Step 1: Setting up a basic web server

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,6 @@ FROM openjdk:${VERSION}-jre
 COPY --from=BUILD /src/build/libs/step-by-step-kotlin-hello-world-jaxrs-all.jar /bin/runner/run.jar
 WORKDIR /bin/runner
 
+EXPOSE 8080
+
 CMD ["java","-jar","run.jar"]
--- src/Server.kt
+++ src/Server.kt
@@ -1,3 +1,28 @@
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory
+import javax.ws.rs.core.UriBuilder
+
 fun main(args: Array<String>) {
-    println("Hello World")
+    val url = UriBuilder.fromUri("http://0.0.0.0/")
+            .port(8080)
+            .build()
+
+    val httpServer = GrizzlyHttpServerFactory.createHttpServer(
+            url,
+            true
+    )
+
+    if (System.getenv()["SHUTDOWN_TYPE"].equals("INPUT")) {
+        println("Press any key to shutdown")
+        readLine()
+        println("Shutting down from input")
+        httpServer.shutdownNow()
+    } else {
+        Runtime.getRuntime().addShutdownHook(Thread {
+            println("Shutting down from shutdown hook")
+            httpServer.shutdownNow()
+        })
+
+        println("Press Ctrl+C to shutdown")
+        Thread.currentThread().join()
+    }
 }
