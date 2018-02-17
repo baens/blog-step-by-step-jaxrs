7c753f0d1ee6d474a2801a805a20fa7fa4db98e6
Erik Lindblom <erik@baens.net>
2018-02-17 16:28:53 -0700

    Step 4: Add logging

--- src/jaxrs/Application.kt
+++ src/jaxrs/Application.kt
@@ -1,9 +1,18 @@
 package jaxrs
 
+import org.glassfish.jersey.logging.LoggingFeature
 import org.glassfish.jersey.server.ResourceConfig
+import java.util.logging.Level
+import java.util.logging.Logger
 
 class Application : ResourceConfig() {
     init {
         packages("jaxrs.resources")
+
+        register(LoggingFeature(Logger.getLogger(LoggingFeature.DEFAULT_LOGGER_NAME),
+                Level.INFO,
+                LoggingFeature.Verbosity.PAYLOAD_ANY,
+                Integer.MAX_VALUE)
+        )
     }
 }
